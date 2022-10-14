ff20e752732b1a3535c5050bca934d6179dade5f
e1drah <113189459+e1drah@users.noreply.github.com>
2022-10-14 15:31:19 -0300

    Fixed pause menu bug

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -15,7 +15,10 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0.25f, 0);
+        if (Time.timeScale != 0)
+        {
+            transform.Rotate(0, 1, 0);
+        }
 
     }
 }
